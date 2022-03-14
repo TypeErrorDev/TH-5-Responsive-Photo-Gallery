7439bdc1bc25f9b556456a7b0e1a5068a7d5b7d7
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-03-14 23:44:02 +0200

    Working on the search bar functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,85 @@
-// Declare Variables
-const search = new Filter("search", "data-caption");
+const searchBar = document.getElementById('search').value;
+const anchor = document.querySelectorAll("a");
+
+
+anchor.forEach( (e, i) =>{
+        let anchorAttr = e.getAttribute("data-caption");
+        if (anchorAttr.includes(searchBar)) {
+           console.log(anchorAttr[i]);
+        } else {
+            anchorAttr[i].style.display = "none";
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Declare Variables
+// const search = new Filter("search", "data-caption");
+
+
+
+// // Set Arrays for photo Title
+// var captions = [
+//   "Hay Bales",
+//   "Lake",
+//   "Canyon",
+//   "Iceberg",
+//   "Desert",
+//   "Fall",
+//   "Plantation",
+//   "Dunes",
+//   "Countryside Lane",
+//   "Sunset",
+//   "Cave",
+//   "Bluebells",
+// ];
+
+// search = document.getElementById("search");
+
+// var render_lists = (lists) => {
+//     var searchBar = "";
+//     for (index in lists) {
+//         search += "<a>" + lists[index] + "</a>";
+//     }
+//     search.innerHTML = searchBar;
+// };
+
+// render_lists(captions);
+
+// search = document.getElementById("search");
+
+// var filterUsers = (e) => {
+//     keyword = input.value.toLowerCase();
+//     filtered_photos = users.filter(function (user) {
+//         user = user.toLowerCase();
+//         return user.indexOf(keyword) > -1;
+//     });
+
+//     render_lists(filtered_users);
+// };
+
+// searchBar.addEventListener("keyup", filterUsers);
