eca7689b6e874d155e7c16c9b130650b689ca151
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-03-17 12:19:37 +0200

    Made search bar function regardless of case

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
-// set const to get the value of the elementID of "search"
-const searchBar = document.getElementById("search").value;
-
-// take the above value, and force lowercase
-input = searchBar.toLowerCase();
-
-// set const to select all the anchor elements
-const anchor = document.querySelectorAll("a");
-
-
 function handleSearch(event) {
-  const currentValue = event.target.value
+  const currentValue = event.target.value.toLowerCase()
   const imageAnchors = document.querySelectorAll('a.photo')
 
   for (const image of imageAnchors) {
